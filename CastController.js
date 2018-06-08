d3c05acd4848c24a79cc9d57a3e38dd3b603c6c4
Hans <jharmannij@cs.ru.nl>
2018-06-08 17:04:16 +0200

    Add License and Readme

--- CastController.js
+++ CastController.js
@@ -1,3 +1,22 @@
+/*
+Chromecast Receiver app for Achtung die Kurve! for Chromecast
+CastController.js takes care of the communication with the sender app
+
+Copyright (C) 2018  Hans Harmannij
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 const CUSTOM_CHANNEL = 'urn:x-cast:nl.harmannij.achtungchromecast';
